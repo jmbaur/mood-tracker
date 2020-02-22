c1f67b67d23fd2df3d354cefd4b28bfae76b98ea
Jared Baur <jaredbaur@fastmail.com>
2020-02-21 22:31:59 -0700

    react-scroll implemented

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Chart.css"
 
 class Chart extends React.Component {
     render() {
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
+import { animateScroll as scroll } from "react-scroll";
 import NoUser from "../NoUser/NoUser.js";
 import Marker from "../Marker/Marker.js";
 import Chart from "../Chart/Chart.js";
+import arrow from "./arrow.svg";
 import "./Main.css";
 
 class Main extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            arrow: "down",
+            topOfPage: true
+        };
+    }
+
+    rotate = () => {
+        if (this.state.arrow === "down") {
+            this.setState({ arrow: "up" });
+        } else {
+            this.setState({ arrow: "down" });
+        }
+    };
+
+    scroll = () => {
+        this.rotate();
+        this.state.topOfPage ? scroll.scrollToBottom() : scroll.scrollToTop();
+        this.setState({ topOfPage: !this.state.topOfPage });
+    };
+
     render() {
         const { loggedIn } = this.props;
         return (
@@ -15,6 +39,13 @@ class Main extends React.Component {
                 ) : (
                     <div>
                         <Marker />
+                        <div className="arrow" onClick={this.scroll}>
+                            <img
+                                src={arrow}
+                                alt="arrow"
+                                id={this.state.arrow}
+                            />
+                        </div>
                         <Chart />
                     </div>
                 )}
