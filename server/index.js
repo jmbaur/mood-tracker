d0b3e829f21de0da611e096f70e03344617051fb
Jared Baur <jaredbaur@fastmail.com>
2020-03-10 15:21:35 -0700

    fix server

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(
         cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }
     })
 );
-// app.use(express.static(`${__dirname}/../build`));
+app.use(express.static(`${__dirname}/../build`));
 
 massive(process.env.CONNECTION_STRING)
     .then(db => {
