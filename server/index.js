d9c564f4fd0cae7cf2deb0cf9d91f0f489321973
Jared Baur <jaredbaur@fastmail.com>
2020-03-11 00:00:51 -0700

    cors

--- server/index.js
+++ server/index.js
@@ -18,6 +18,14 @@ app.use(
         cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }
     })
 );
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(express.static(`${__dirname}/../build`));
 
 massive(process.env.CONNECTION_STRING)
