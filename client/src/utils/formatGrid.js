f5f2cbcab5b7f2b8d28557d81d30b577ecc26c8d
Jared Baur <jaredbaur@fastmail.com>
2020-05-13 18:55:39 +0000

    More Grid Bug Fixes
    Grid was not displaying date for last day, but it now is.

--- client/src/utils/formatGrid.js
+++ client/src/utils/formatGrid.js
@@ -7,33 +7,52 @@ export default function formatGrid(marks, moods, start, end) {
   let tmpMark = cleanTmpMark();
 
   // reduce marks into individual days
-  for (let i = 0; i < marks.length - 1; i++) {
+  for (let i = 0; i < marks.length; i++) {
     let doy0 = +moment(marks[i].time).format("DDD");
-    let doy1 = +moment(marks[i + 1].time).format("DDD");
 
-    if (doy0 === doy1) {
-      // same day
-      tmpMark.count++;
-      tmpMark.mood += marks[i].number;
-      tmpMark.doy = doy0;
-      tmpMark.viewDate = moment(marks[i].time).format("ddd MMM DD");
-      tmpMark.fullDate = moment(marks[i].time)
-        .startOf("day")
-        .format();
+    // not on last mark
+    if (i !== marks.length - 1) {
+      let doy1 = +moment(marks[i + 1].time).format("DDD");
+
+      if (doy0 === doy1) {
+        tmpMark.count++;
+        tmpMark.mood += marks[i].number;
+      } else {
+        // current mark's day does not equal next mark's day
+        tmpMark.count++;
+        tmpMark.color =
+          colors[(Math.round(tmpMark.mood / tmpMark.count) || 1) - 1];
+        tmpMark.viewDate = moment(marks[i].time).format("ddd MMM DD");
+        tmpMark.fullDate = moment(marks[i].time)
+          .startOf("day")
+          .format();
+        tmpArr.push(tmpMark);
+        tmpMark = cleanTmpMark();
+      }
     } else {
-      // new day
-      tmpMark.color =
-        colors[(Math.round(tmpMark.mood / tmpMark.count) || 1) - 1];
+      let doy1 = +moment(marks[i - 1]?.time).format("DDD");
+      if (doy0 === doy1) {
+        tmpMark.mood += marks[i].number;
+        tmpMark.count++;
+        tmpMark.color =
+          colors[(Math.round(tmpMark.mood / tmpMark.count) || 1) - 1];
+        tmpMark.viewDate = moment(marks[i].time).format("ddd MMM DD");
+        tmpMark.fullDate = moment(marks[i].time)
+          .startOf("day")
+          .format();
+      } else {
+        tmpMark = {
+          count: 1,
+          color: colors[marks[i].number - 1],
+          viewDate: moment(marks[i].time).format("ddd MMM DD"),
+          fullDate: moment(marks[i].time)
+            .startOf("day")
+            .format()
+        };
+      }
       tmpArr.push(tmpMark);
-      tmpMark = cleanTmpMark();
     }
   }
-
-  // generate info for last tmpMark
-  tmpMark.color = colors[(Math.round(tmpMark.mood / tmpMark.count) || 1) - 1];
-  tmpMark.count++;
-  // push on last tmpMark
-  tmpArr.push(tmpMark);
 
   fill(tmpArr);
   return tmpArr;
